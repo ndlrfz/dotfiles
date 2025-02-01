9399d50d4f6f4c4651ade9a7d34e8545faa08b03
ndlrfz <arvdl64@gmail.com>
2025-02-01 19:04:12 +0700

    Add wezterm

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,34 @@
+local wezterm = require('wezterm')
+local config = wezterm.config_builder()
+local mux = wezterm.mux
+
+wezterm.on('gui-startup', function()
+   local _, _, window = mux.spawn_window({})
+   window:gui_window():maximize()
+end)
+
+config.font = wezterm.font('Iosevka SS14', { weight = 'Regular' })
+config.freetype_load_target = 'Normal'
+config.font_size = 14.9
+config.line_height = 1.1
+-- config.colors = require('cyberdream')
+-- config.color_scheme = " Everforest Dark (Gogh) Monokai Soda tokyonight_moon tokyonight_day Espresso Espresso Libre"
+config.color_scheme = 'Everforest Dark (Gogh)'
+config.warn_about_missing_glyphs = false
+
+-- spawn zellij as the default shell
+config.default_prog = { '/usr/bin/tmux' }
+
+config.enable_tab_bar = false
+config.window_decorations = 'RESIZE'
+config.window_background_opacity = 0.85
+
+local padding = 5
+config.window_padding = {
+   top = padding,
+   left = padding,
+   right = padding,
+   bottom = padding,
+}
+
+return config
