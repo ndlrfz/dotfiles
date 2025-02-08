a77bc3aa310a1d6ebdeca3ea79701b78efac5f25
ndlrfz <arvdl64@gmail.com>
2025-02-08 10:01:28 +0700

    Update

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,34 +1,35 @@
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
+-- test
 local config = wezterm.config_builder()
 local mux = wezterm.mux
 
-wezterm.on('gui-startup', function()
-   local _, _, window = mux.spawn_window({})
-   window:gui_window():maximize()
+wezterm.on("gui-startup", function()
+	local _, _, window = mux.spawn_window({})
+	window:gui_window():maximize()
 end)
 
-config.font = wezterm.font('Iosevka SS14', { weight = 'Regular' })
-config.freetype_load_target = 'Normal'
-config.font_size = 14.9
+config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Regular" })
+config.freetype_load_target = "Normal"
+config.font_size = 13.9
 config.line_height = 1.1
 -- config.colors = require('cyberdream')
 -- config.color_scheme = " Everforest Dark (Gogh) Monokai Soda tokyonight_moon tokyonight_day Espresso Espresso Libre"
-config.color_scheme = 'Everforest Dark (Gogh)'
+config.color_scheme = "Everforest Dark (Gogh)"
 config.warn_about_missing_glyphs = false
 
 -- spawn zellij as the default shell
-config.default_prog = { '/usr/bin/tmux' }
+config.default_prog = { "/usr/bin/zsh" }
 
 config.enable_tab_bar = false
-config.window_decorations = 'RESIZE'
-config.window_background_opacity = 0.85
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.95
 
 local padding = 5
 config.window_padding = {
-   top = padding,
-   left = padding,
-   right = padding,
-   bottom = padding,
+	top = padding,
+	left = padding,
+	right = padding,
+	bottom = padding,
 }
 
 return config
